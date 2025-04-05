41df0bec729559b1586a2a520805aa35f3981250
Erudite885 <erudite885@gmail.com>
2025-04-05 03:10:14 +0100

    index init

---
+++ src/components/pages/index.js
@@ -0,0 +1,17 @@
+import ChooseDateStep from "./chooseDateStep/ChooseDateStep";
+import PaymentStep from "./paymentStep/PaymentStep";
+import PermitCheckStep from "./permitCheckStep/PermitCheckStep";
+import PostcodeStep from "./postcodeStep/PostcodeStep";
+import SelectSkipButton from "../ui/selectSkipButton/SelectSkipButton";
+import WasteTypeStep from "./wasteTypeStep/WasteTypeStep";
+import SelectSkipStep from "./selectSkipStep/SelectSkipStep";
+
+export {
+  SelectSkipButton,
+  WasteTypeStep,
+  ChooseDateStep,
+  PaymentStep,
+  PermitCheckStep,
+  PostcodeStep,
+  SelectSkipStep,
+};
