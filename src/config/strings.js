1c11ad2056a09dad6d94a3e7a7dcc3f018d085b6
Erudite885 <erudite885@gmail.com>
2025-04-05 03:10:42 +0100

    string constants

---
+++ src/config/strings.js
@@ -0,0 +1,44 @@
+export const strings = {
+  title: "Choose Your Skip Size",
+  subtitle: "Select the skip size that best suits your needs",
+  heavyWasteError: "Not Suitable for Heavy Waste",
+  privatePropertyWarning: "Private Property Only",
+  imageURL:
+    "https://images.unsplash.com/photo-1590496793929-36417d3117de?q=80&w=800",
+  backButton: "Back",
+  continueButton: "Continue",
+  selected: "Selected",
+  selectThisSkip: "Select This Skip",
+  plusVAT: " 20% VAT inclusive",
+  currency: "£",
+  perWeek: "per week",
+  dayHirePeriod: "day hire period",
+  yardSkip: "Yard Skip",
+  yardSkipPopup: "Yard Skip - ",
+  postcodeLabel: "Postcode",
+  wasteTypeLabel: "Waste Type",
+  selectSkipLabel: "Select Skip",
+  permitCheckLabel: "Permit Check",
+  chooseDateLabel: "Choose Date",
+  paymentLabel: "Payment",
+ };
+
+export const API_URL = 'https://app.wewantwaste.co.uk/api/skips/by-location?postcode=NR32&area=Lowestoft'
+
+export const COLORS = {
+    white: '#fff',
+    black: '#000',
+    blue: '#0037C1',
+    blueHover: '#002DA1',
+    subtitle: '#969CA8',
+    backButton: '#2A2A2A',
+    backButtonHover: '#3A3A3A',
+    warning: '#E4AF07',
+    error: '#E84242',
+    disabled: '#555',
+    selectSkipButtonColor: '#333',
+    selectSkipButtonHover: '#444',
+    selectSkipButtonDisabledText: '#888',
+    backgroundColor: '#1E1E1E'
+
+}
